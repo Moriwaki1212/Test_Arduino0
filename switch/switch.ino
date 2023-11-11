9a4deb36209e3a8f7efc7e76b763d0393ce17821
Moriwaki Hiroto <xhia.doneru.19951212.m@gmail.com>
2023-11-11 14:34:51 +0900

    analogWrite

--- switch/switch.ino
+++ switch/switch.ino
@@ -1,17 +1,22 @@
+
+int led = 5;
+int duty = 255; 
+
+
 void setup() {
     delay(10000);
     pinMode(3, INPUT);              //3ピンを入力ピンにする
-    pinMode(7, OUTPUT);             //7ピンを出力ピンにする
+    pinMode(led, OUTPUT);             //7ピンを出力ピンにする
 
 }
 
 void loop() {
     if(digitalRead(3)== HIGH){      //もし3ピンがHIGHなら
-      digitalWrite(7,1);
+      analogWrite(led,duty);
       delay(5000);            //7ピンをHIGHにする
     }
     else {                          //もし3ピンがLOWなら
-      digitalWrite(7,0);            //7ピンをLOWにする
+      analogWrite(led,0);            //7ピンをLOWにする
     }
 
 }
